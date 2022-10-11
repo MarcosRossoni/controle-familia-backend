2c124a26141262029429a722a6fd2106e9ea6578
marcosrossoni <marcosrossoni9@gmail.com>
2022-10-10 23:33:39 -0300

    ajuste

--- CursoSpringBootAngularBackend/src/main/java/br/com/cursospringbootangularbackend/model/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.cursospringbootangularbackend.model.repository;
-
-import br.com.cursospringbootangularbackend.model.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
